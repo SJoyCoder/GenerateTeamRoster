b9dc623c132915a81be1cc3cf58410f499a92b05
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-25 15:41:22 -0500

    made HTML base

--- index.js
+++ index.js
@@ -1,76 +1,85 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 
-const teamInput = [
-    {
-        type: "input",
-        name: "name",
-        message: "Please input Team Manager's name."
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Please input Team Manager's Employee Id."
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Please input Team Manager's email."
-    }, 
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "Please input Team Manager's office number."
-    },
-    {
-        type: "list",
-        name: "employeeType",
-        message: "Do you want to input an Engineer or an Intern?",
-        choices: ["Engineer", "Intern"]
-    }
-];
-
-function internEngineer(){
-    let employeeType = answers.employeeType;
-    if(employeeType === "Intern"){
-        teamInput = [
-            {
-                type: "input",
-                name: "school",
-                message: "What school does the Intern attend?"
-            }
-
-        ];
-    }else if(employeeType === "Engineer"){
-        teamInput = [{
+const questions = 
+[
+        {
             type: "input",
-            name: "github",
-            message: "What is the link to the Engineer's GitHub?"
-        }
+            name: "name",
+            message: "Please input Team Manager's name."
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "Please input Team Manager's Employee Id."
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please input Team Manager's email."
+        }, 
+        {
+            type: "number",
+            name: "officeNumber",
+            message: "Please input Team Manager's office number."
+        },
+        {
+            type: "list",
+            name: "employeeType",
+            message: "Do you want to input an Engineer or an Intern?",
+            choices: ["Engineer", "Intern"]
+        },
+
+        // internEngineer()
     ];
-    }
-}
-internEngineer();
 
-function writeTOFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err){
+    
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, function(err){
+            
+            
+        })
+    };
+    
+    // function internEngineer(){
+    //     let employeeType = employeeType.answers;
+    //     if(employeeType === "Intern"){
+    //         questions = [
+    //             {
+    //                 type: "input",
+    //                 name: "school",
+    //                 message: "What school does the Intern attend?"
+    //             }
+    
+    //         ];
+    //     }else if(employeeType === "Engineer"){
+    //         questions = [{
+    //             type: "input",
+    //             name: "github",
+    //             message: "What is the link to the Engineer's GitHub?"
+    //         }];
+    //     }
+    // };
+    function init() {
+        console.log("yay");
+        inquirer.prompt(questions).then(function(answers){
+        
+        
+        let HTMLContent = `<h1>Team Roster</h1>
 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        `;
+        writeToFile("../dist/renderedHTML.HTML", HTMLContent);
     })
-};
-
-function init() {
-    console.log("yay");
-    inquirer.prompt(teamInput).then(function(answers){
-        let output;
-let HTMLContent = `<h1>Team Roster</h1>
-
-
-
-
-
-
-
-
-`
-    })
+    console.table(answers);
 }
+
+init();
